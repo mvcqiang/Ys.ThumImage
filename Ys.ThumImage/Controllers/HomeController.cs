ee8faaa4e9e4cd5a050b1fca7468ed2887d42288
mvcqiang <1024347665@qq.com>
2016-09-03 11:52:40 +0800

    删除旧的代码

--- Ys.ThumImage/Controllers/HomeController.cs
+++ Ys.ThumImage/Controllers/HomeController.cs
@@ -64,60 +64,6 @@ namespace Ys.ThumImage.Controllers
             return File(pPath, FileHelper.GetContentTypeByExtension(Path.GetExtension(pPath).ToLower()));
         }
 
-        #region old
-        /// <summary>
-        /// 生成图片缩率图Action
-        /// </summary>
-        /// <param name="p">原图url</param>
-        /// <param name="id">图片尺寸以及生成缩率图的类型</param>
-        /// <returns></returns>
-        //[HttpGet]
-        //public ActionResult Index(string p, string id)
-        //{
-        //    if (string.IsNullOrEmpty(p))
-        //    {
-        //        return new HttpStatusCodeResult(404);
-        //    }
-
-
-
-        //    string oPath = Regex.Replace(p, @"http[s]?://(.*?)/", "/", RegexOptions.IgnoreCase);
-        //    int? oWidth = 200, oHeight = 200;
-        //    int cutMode = 3;
-        //    string pPath;
-        //    string oDir;
-
-        //    if (!string.IsNullOrEmpty(id))
-        //    {
-        //        string[] ss = id.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
-        //        if (ss.Length < 2)
-        //        {
-        //            return new HttpStatusCodeResult(404);
-        //        }
-        //        if (ss.Length > 2)
-        //        {
-        //            cutMode = int.Parse(ss[2]);
-        //        }
-        //        oPath = oPath.Insert(oPath.LastIndexOf('/') + 1, string.Format("{0}_{1}_{2}_", ss[0], ss[1], cutMode));
-        //        oWidth = int.Parse(ss[0]);
-        //        oHeight = int.Parse(ss[1]);
-        //    }
-
-        //    pPath = Server.MapPath(oPath);
-        //    oDir = Path.GetDirectoryName(pPath);
-
-        //    if (!System.IO.File.Exists(pPath))
-        //    {
-        //        byte[] imagebytes = FileHelper.DownLoadFile(p);
-        //        if (!Directory.Exists(oDir))
-        //        {
-        //            Directory.CreateDirectory(oDir);
-        //        }
-        //        FileHelper.MakeThumbnail(FileHelper.BytToImg(imagebytes), oWidth.Value, oHeight.Value, (ThumbnailMode)cutMode, pPath, true);
-        //    }
-
-        //    return File(pPath, FileHelper.GetContentTypeByExtension(Path.GetExtension(pPath).ToLower()));
-        //}
-        #endregion
+       
     }
 }
